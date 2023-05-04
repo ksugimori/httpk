300b5845e13bb20f7454e511e41c1812befa30b4
ksugimori <ksugimori.private@gmail.com>
2023-05-04 19:32:00 +0900

    pass Socket as argument

--- app/src/main/kotlin/httpk/handler/EchoHandler.kt
+++ app/src/main/kotlin/httpk/handler/EchoHandler.kt
@@ -1,24 +1,47 @@
 package httpk.handler
 
 import httpk.log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.PrintWriter
 import java.net.Socket
 
-class EchoHandler : Handler {
-    override fun handle(socket: Socket) {
-        log("connected from ${socket.inetAddress}")
+class EchoHandler() : Handler {
+    override suspend fun handle(socket: Socket) {
+        log("connected from: ${socket.inetAddress}")
 
-        val reader = socket.getInputStream().bufferedReader()
-        val writer = PrintWriter(socket.getOutputStream().bufferedWriter())
+        socket.use {
+            val reader = getBufferedReaderSuspend(it)
+            val writer = PrintWriter(getBufferedWriterSuspend(it))
 
-        do {
-            val line = reader.readLine()
-            log("got \"$line\"")
-            writer.println(line)
-            writer.flush()
+            do {
+                val line = readLineSuspend(reader)
+                log("got \"$line\"")
+                writer.println(line)
+                writer.flush()
 
-            if (line.isBlank()) break
-        } while (true)
+                if (line.isBlank()) break
+            } while (true)
+        }
+
+        log("close connection: ${socket.inetAddress}")
+    }
+
+    //
+    // private
+    //
+    private suspend fun getBufferedReaderSuspend(socket: Socket): BufferedReader {
+        return withContext(Dispatchers.IO) { socket.getInputStream() }.bufferedReader()
+    }
+
+    private suspend fun getBufferedWriterSuspend(socket: Socket): BufferedWriter {
+        return withContext(Dispatchers.IO) { socket.getOutputStream() }.bufferedWriter()
+    }
+
+    private suspend fun readLineSuspend(reader: BufferedReader): String {
+        return withContext(Dispatchers.IO) { reader.readLine() }
     }
 
 }
