c1c6b7a88812a0afe366db8d32954a37d141112e
ksugimori <12605906+ksugimori@users.noreply.github.com>
2023-05-09 22:42:11 +0900

    Add RequestBuilder

--- app/src/main/kotlin/httpk/handler/HttpHandler.kt
+++ app/src/main/kotlin/httpk/handler/HttpHandler.kt
@@ -1,5 +1,6 @@
 package httpk.handler
 
+import httpk.core.HttpRequest
 import httpk.core.RequestLine
 import httpk.core.parseRequestLine
 import httpk.log
@@ -10,32 +11,55 @@ import java.io.BufferedWriter
 import java.io.PrintWriter
 import java.net.Socket
 
-class HttpHandler() : Handler {
+private class RequestBuilder() {
+    var requestLine: RequestLine? = null
+    var headers: MutableList<String> = mutableListOf()
 
-    private sealed class State(val value: String) {
-        abstract fun process(line: String): State
-        override fun toString(): String = value
+    fun build(): HttpRequest {
+        if (requestLine == null) throw IllegalStateException("requestLine not initialized")
 
-        object Init : State("INIT") {
-            override fun process(line: String): State {
-                val requestLine = parseRequestLine(line)
-                log("Request: $requestLine")
+        return HttpRequest(
+            method = requestLine!!.method,
+            path = requestLine!!.path,
+            version = requestLine!!.version,
+            headers = headers,
+            body = null
+        )
+    }
+}
 
-                return State.Header
-            }
-        }
+private sealed class State(val value: String) {
+    // TODO State の中に builder を持つと static にせざるを得ないので気持ち悪い
+    companion object {
+        val builder: RequestBuilder = RequestBuilder()
+    }
+    abstract fun process(line: String): State
+    override fun toString(): String = value
 
-        object Header : State("HEADER") {
-            override fun process(line: String): State {
-                if (line.isBlank()) return State.End
-                return State.Header
-            }
-        }
-
-        object End : State("END") {
-            override fun process(line: String): State = State.End
+    object Init : State("INIT") {
+        override fun process(line: String): State {
+            builder.requestLine = parseRequestLine(line)
+            return State.Header
         }
     }
+
+    object Header : State("HEADER") {
+        override fun process(line: String): State {
+            return if (line.isBlank()) {
+                State.End
+            } else {
+                builder.headers.add(line)
+                State.Header
+            }
+        }
+    }
+
+    object End : State("END") {
+        override fun process(line: String) = State.End
+    }
+}
+
+class HttpHandler() : Handler {
 
     override suspend fun handle(socket: Socket) {
         var state: State = State.Init
@@ -49,6 +73,9 @@ class HttpHandler() : Handler {
                 // TODO リクエストボディのビルダーを引数に（もしくはビルダー内に state を持たせる）
                 state = state.process(line)
             }
+
+            val request = State.builder.build()
+            log("Request: $request")
 
             // TODO ドキュメント取得
             // TODO Response クラス作成
