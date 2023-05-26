2ea01dbc643353be052739aee7e3f1b718a08fdc
ksugimori <12605906+ksugimori@users.noreply.github.com>
2023-05-26 09:41:20 +0900

    HttpHandler を関数型に変更

--- app/src/main/kotlin/httpk/handler/HttpHandler.kt
+++ app/src/main/kotlin/httpk/handler/HttpHandler.kt
@@ -3,6 +3,4 @@ package httpk.handler
 import httpk.core.message.HttpRequest
 import httpk.core.message.HttpResponse
 
-interface HttpHandler {
-    fun handle(request: HttpRequest): HttpResponse
-}
+typealias HttpHandler = (request: HttpRequest) -> HttpResponse
