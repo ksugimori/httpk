18416834642752cca887629857e3dab4a480b5a4
ksugimori <12605906+ksugimori@users.noreply.github.com>
2023-05-26 09:32:48 +0900

    HttpHandler をコンストラクタで渡せるように変更

--- app/src/main/kotlin/httpk/core/message/HttpStatus.kt
+++ app/src/main/kotlin/httpk/core/message/HttpStatus.kt
@@ -2,6 +2,7 @@ package httpk.core.message
 
 enum class HttpStatus(val code: Int, val message: String) {
     OK(200, "OK"),
+    CREATED(201, "Created"),
     BAD_REQUEST(400, "Bad Request"),
     NOT_FOUND(404, "Not Found"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
