ee86e08c90e48025e3966b314eec1bf9f7ca295c
ksugimori <ksugimori.private@gmail.com>
2023-06-24 08:49:04 +0900

    Add PrintWriter#printAll

--- app/src/main/kotlin/httpk/core/io/HttpWriter.kt
+++ app/src/main/kotlin/httpk/core/io/HttpWriter.kt
@@ -7,26 +7,28 @@ import java.io.PrintWriter
 private const val SP = " "
 private const val CRLF = "\r\n"
 
+/**
+ * HTTP レスポンスを書き出す Writer
+ *
+ * @param outputStream 出力先ストリーム
+ */
 class HttpWriter(private val outputStream: OutputStream) {
 
+    /**
+     * HTTP レスポンスを書き出す。
+     *
+     * @param response HTTP レスポンス
+     */
     fun writeResponse(response: HttpResponse) {
         val writer = PrintWriter(outputStream)
 
         // status line
-        writer.print(response.version)
-        writer.print(SP)
-        writer.print(response.status.code)
-        writer.print(SP)
-        writer.print(response.status.message)
-        writer.print(CRLF)
+        writer.printAll(response.version, SP, response.status.code, SP, response.status.message, CRLF)
 
         // headers
-        for ((fieldName, fieldValues) in response.headers) {
-            writer.print(fieldName)
-            writer.print(":")
-            writer.print(SP)
-            writer.print(fieldValues.joinToString(", "))
-            writer.print(CRLF)
+        for ((fieldName, valueList) in response.headers) {
+            val fieldValue = valueList.joinToString(", ")
+            writer.printAll(fieldName, ":", SP, fieldValue, CRLF)
         }
 
         // ヘッダーとボディの区切りの空行
@@ -37,4 +39,11 @@ class HttpWriter(private val outputStream: OutputStream) {
         // body
         outputStream.write(response.body)
     }
+
+    /**
+     * 可変長引数で受け取ったオブジェクトを全て書き出す。
+     *
+     * @param objects 出力するオブジェクト
+     */
+    private fun PrintWriter.printAll(vararg objects: Any) = objects.forEach(this::print)
 }
