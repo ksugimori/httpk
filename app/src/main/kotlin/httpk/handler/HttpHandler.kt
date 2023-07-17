16987f0da1c70f881bc6248c25eeeff9d399d61a
ksugimori <12605906+ksugimori@users.noreply.github.com>
2023-07-17 21:38:44 +0900

    HttpHandler を interface に変更

--- app/src/main/kotlin/httpk/handler/HttpHandler.kt
+++ app/src/main/kotlin/httpk/handler/HttpHandler.kt
@@ -6,36 +6,14 @@ import httpk.http.semantics.*
 /**
  * HTTP ハンドラ。
  */
-abstract class HttpHandler {
+interface HttpHandler {
 
     /**
      * HTTP リクエストの処理を行う。
      *
-     * 例外はすべてキャッチされ、必ずレスポンスが返ります。
      * @param request HTTP リクエスト
      * @return HTTP レスポンス
      */
-    fun handle(request: HttpRequest): HttpResponse {
-        return try {
-            doHandle(request)
-        } catch (ex: ResourceNotFoundException) {
-            val body = "<!DOCTYPE html><html><body><h1>NOT FOUND</h1></body></html>".toByteArray()
-            val headers = HttpHeaders {
-                contentType = MimeType.HTML
-                contentLength = body.size
-            }
-            HttpResponse(status = HttpStatus.NOT_FOUND, headers = headers, body = body)
-        } catch (ex: Exception) {
-            HttpResponse(status = HttpStatus.BAD_REQUEST)
-        }
-    }
-
-    /**
-     * HTTP リクエストの処理の拡張ポイント。
-     *
-     * @param request HTTP リクエスト
-     * @return HTTP レスポンス
-     */
-    protected abstract fun doHandle(request: HttpRequest): HttpResponse
+    fun handle(request: HttpRequest): HttpResponse
 }
 
